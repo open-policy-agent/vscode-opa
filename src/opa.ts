open-policy-agent/vscode-opa

commit 0dfeed45eb71cd9fe0173b174005ed898d1b6658

feat: add configuraton to set environment variables (#103)
Signed-off-by: Max Stephan <dev@mxsph.de>

src/opa.ts
@@ -300,6 +300,10 @@ function getOpaPath(context: vscode.ExtensionContext | undefined, path: string, 
     }
 }
 
+function getOpaEnv(): NodeJS.ProcessEnv {
+    return vscode.workspace.getConfiguration('opa').get<NodeJS.ProcessEnv>('env', {});
+}
+
 // runWithStatus executes the OPA binary at path with args and stdin. The
 // callback is invoked with the exit status, stderr, and stdout buffers.
 export function runWithStatus(context: vscode.ExtensionContext | undefined, path: string, args: string[], stdin: string, cb: (code: number, stderr: string, stdout: string) => void) {
@@ -310,7 +314,7 @@ export function runWithStatus(context: vscode.ExtensionContext | undefined, path
 
     console.log("spawn:", opaPath, "args:", args.toString());
 
-    let proc = cp.spawn(opaPath, args);
+    let proc = cp.spawn(opaPath, args, {env: getOpaEnv()});
 
     proc.stdin.write(stdin);
     proc.stdin.end();

commit 5c39a6f0251fbb1e3cf7b02d10de9550d02019df

fix(runWithStatus): execution falls on some systems
caused by #103
fixes #107

Signed-off-by: Max Stephan <dev@mxsph.de>

src/opa.ts
@@ -314,7 +314,7 @@ export function runWithStatus(context: vscode.ExtensionContext | undefined, path
 
     console.log("spawn:", opaPath, "args:", args.toString());
 
-    let proc = cp.spawn(opaPath, args, {env: getOpaEnv()});
+    let proc = cp.spawn(opaPath, args, {env: {...process.env, ...getOpaEnv()}});
 
     proc.stdin.write(stdin);
     proc.stdin.end();